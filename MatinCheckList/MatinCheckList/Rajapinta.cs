9263929712073d410c59319e1dc36366eb609783
Pegasos64 <jyrsy@live.fi>
2018-05-26 18:50:16 +0300

    rajapinta

---
+++ MatinCheckList/MatinCheckList/Rajapinta.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace MatinCheckList
+{
+    class Rajapinta
+    {
+        private string _server = "88.195.42.239";
+        private string _database = "matinchecklist";
+
+
+        private string connectionString;
+        private MySqlConnection connection;
+
+        private void Login(string user, string password)
+        {
+            connectionString = 
+                "SERVER=" + _server + 
+                ";DATABASE=" + _database + 
+                ";UID=" + user + 
+                ";PASSWORD=" + password + 
+                ";";
+            connection = new MySqlConnection(connectionString + "SSLMode=none");
+
+        }
+
+        public Rajapinta()
+        {
+
+        }
+
+
+
+        #region utilities
+        /// <summary>
+        /// Diagnoses the connection to server
+        /// </summary>
+        /// <returns>errorCode, -1 if succesfull</returns>
+        private int DiagnoseConnection()
+        {
+            try
+            {
+                connection.Open();
+                connection.Close();
+                return -1;
+            }
+            catch (MySqlException ex)
+            {
+                return ex.Number;
+            }
+        }
+
+        /// <summary>
+        /// Opens the connection for entering commands to the server
+        /// </summary>
+        /// <returns>boolean true if succesfull, false if not</returns>
+        private bool OpenConnection()
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Open) return true;
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("" + ex.Message);
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Closes the connection to the server
+        /// </summary>
+        /// <returns></returns>
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+
+        #endregion
+
+
+    }
+}
